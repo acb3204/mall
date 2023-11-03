343b4b3d0e5bbb3bdb4890ac4f23112a7e983e3c
DESKTOP-L8AQJCQ\acb32 <acb3204@naver.com>
2023-11-03 18:07:50 +0900

    ㄴㄴ

---
+++ mall/src/main/java/config/CtrlConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import ctrl.*;
+import svc.*;
+
+@Configuration
+public class CtrlConfig {	// 구현해 놓은 컨트롤러들을 스프링 빈으로 등록 시키는 클래스
+	
+}
---
+++ mall/src/main/java/vo/IndexCtrl.java
@@ -0,0 +1,12 @@
+package vo;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IndexCtrl {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
